1c1f7b89be1072ff67b79547d82fb31dd7acb313
Yuri <ycr.data@gmail.com>
2024-07-18 00:05:26 -0300

    turning zip_code optional to show endpoint

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root 'zip_codes#index'
+  root 'zip_codes#index'
 
-  get '/zip_codes/:zip_code', to: 'zip_codes#show', as: 'zip_code'
+  get '/zip_codes(/:zip_code)', to: 'zip_codes#show', as: 'zip_code'
 end
