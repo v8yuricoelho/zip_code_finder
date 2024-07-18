893f08b31fd00f23148f0cf313532f47b228ec9f
Yuri <ycr.data@gmail.com>
2024-07-18 00:06:58 -0300

    importing tailwind script (deleting comments this time)

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -20,19 +20,3 @@ module.exports = {
     require('@tailwindcss/container-queries'),
   ]
 }
-
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     './public/*.html',
-//     './app/helpers/**/*.rb',
-//     './app/javascript/**/*.js',
-//     './app/views/**/*',
-//   ],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [
-//     require('@tailwindcss/forms'),
-//   ],
-// }
