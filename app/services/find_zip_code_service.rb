e54822cbee6db1ccb477d6296e89a07118a97225
Yuri <ycr.data@gmail.com>
2024-07-21 23:14:41 -0300

    refactoring find_zip_code_service

--- app/services/find_zip_code_service.rb
+++ app/services/find_zip_code_service.rb
@@ -6,27 +6,30 @@ class FindZipCodeService
   end
 
   def call
-    response = Faraday.get("https://cep.awesomeapi.com.br/json/#{@zip_code}")
-    return process_zip_code(response) if response.success?
+    @response = Faraday.get("https://cep.awesomeapi.com.br/json/#{@zip_code}")
+    return process_zip_code if @response.success?
 
-    { 'error_message' => JSON.parse(response.body)['message'] }
+    { 'error_message' => JSON.parse(@response.body)['message'] }
   end
 
   private
 
-  def process_zip_code(response)
-    parsed_response = JSON.parse(response.body)
-
-    zip_code = ZipCode.find_or_create_by(
-      cep: parsed_response['cep'],
-      address: parsed_response['address'],
-      district: parsed_response['district'],
-      state: parsed_response['state'],
-      city: parsed_response['city'],
-      ddd: parsed_response['ddd']
-    )
+  def process_zip_code
+    zip_code = ZipCode.find_or_initialize_by(
+      cep: parsed_response['cep']
+    ).tap do |object|
+      object.address = parsed_response['address']
+      object.district = parsed_response['district']
+      object.state = parsed_response['state']
+      object.city = parsed_response['city']
+      object.ddd = parsed_response['ddd']
+    end
 
     zip_code.update(search_count: zip_code.search_count + 1)
     zip_code.attributes
+  end
+
+  def parsed_response
+    @parsed_response ||= JSON.parse(@response.body)
   end
 end
